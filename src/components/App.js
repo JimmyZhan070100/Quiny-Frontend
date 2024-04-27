2232dadd22b6c9e580f28022ac207ca0a2e01157
Jimmycofer <jimmy000701@gmail.com>
2024-04-26 21:39:34 -0500

    Finish Google Login

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,51 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import URLShortenerForm from "./URLShortenerForm";
 import "./App.css";
-import GoogleLogin from "react-google-login";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
 
-function App() {
+const LoginButton = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate(); // Now it's inside a component that is a child of <Router>
+
   const handleGoogleLogin = (response) => {
-    // Handle the successful Google login response
     console.log(response);
+    navigate("/"); // Navigate to the homepage on successful login
   };
 
   const handleGoogleLoginFailure = (error) => {
-    // Handle the Google login failure
     console.error(error);
   };
 
+  return (
+    <GoogleLogin
+      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} // Add Cliend ID in .env file
+      buttonText="Login with Google"
+      onSuccess={handleGoogleLogin}
+      onFailure={handleGoogleLoginFailure}
+      cookiePolicy={"single_host_origin"}
+    />
+  );
+};
+
+function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <div className="brand">
-            <a href="/" className="home-link">
+            <Link to="/" className="home-link">
               <h1>Quiny</h1>
-            </a>
+            </Link>
           </div>
           <nav className="navigation">
-            <GoogleLogin
-              clientId="YOUR_GOOGLE_CLIENT_ID"
-              buttonText="Login with Google"
-              onSuccess={handleGoogleLogin}
-              onFailure={handleGoogleLoginFailure}
-              cookiePolicy={"single_host_origin"}
-            />
+            <LoginButton /> {/* Use the new LoginButton component */}
             <button className="register">Register Now</button>
           </nav>
         </header>
